06c9c11252dc093a354ed59e20678e59fd80fb08
DESKTOP-JFG7H38\MorganKim <kjy2696@gmail.com>
2021-07-21 11:21:50 +0900

    [김지홍] 최초 파일 추가 example

---
+++ src/main/java/com/morganj/spring/lesson01/Data.java
@@ -0,0 +1,21 @@
+package com.morganj.spring.lesson01;
+
+public class Data {
+	//id , 이름
+	private int id ;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/main/java/com/morganj/spring/lesson01/Ex01RestController.java
@@ -0,0 +1,53 @@
+package com.morganj.spring.lesson01;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@Controller + @ ResponseBody 가 합쳐진거
+@RequestMapping("/lesson01/ex01")
+public class Ex01RestController {
+	
+	@RequestMapping("/3")
+	public String printString() {
+		return "@RestController 사용";
+	}
+	
+	@RequestMapping("/4")
+	public Map<String , String> printMap(){
+		Map<String , String> map = new HashMap<>();
+		
+		map.put("김인규", "강사");
+		map.put("유재석", "개그맨");
+		map.put("아이유", "가수");
+		return map;
+	}
+	
+	@RequestMapping("/5")
+	public Data printData() {
+		Data data = new Data();
+		data.setId(1);
+		data.setName("김인규");
+		
+		return data;
+	}
+	
+	//리스폰스 엔티디
+	@RequestMapping("/6")
+	public ResponseEntity<Data> entity(){
+		Data data = new Data();
+		data.setId(1);
+		data.setName("김인규");
+		
+		ResponseEntity<Data> responseEntity = new ResponseEntity<>(data, HttpStatus.INTERNAL_SERVER_ERROR);
+		
+		return responseEntity;
+	}
+	
+	
+}
